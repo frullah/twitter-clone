96587cb4e89e79de6eb38642d2c572eec3b87195
Fajarullah <frullah12@gmail.com>
2021-11-20 13:25:33 +0700

    use windicss to make styling compilation faster

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,2 +1,4 @@
-import "@hotwired/turbo-rails"
+import 'virtual:windi.css'
 import '~/stylesheets/application.css'
+
+import "@hotwired/turbo-rails"
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
+import WindiCSS from 'vite-plugin-windicss'
+import colors from 'windicss/colors'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    FullReload(['config/routes.rb', 'app/views/**/*.html.erb'])
+    FullReload(['config/routes.rb', 'app/views/**/*.html.erb']),
+    WindiCSS({
+      config: {
+        extract: {
+          include: [
+            "./app/views/**/*.html.erb"
+          ]
+        },
+        // purge: [
+        // ],
+        darkMode: false, // or 'media' or 'class'
+        theme: {
+          extend: {
+            colors: {
+              primary: colors.blue,
+              hover: colors.blue[800],
+            }
+          },
+        },
+        variants: {
+          // extend: {},
+        },
+        plugins: [],
+        preflight: true
+      }
+    })
   ],
 })
