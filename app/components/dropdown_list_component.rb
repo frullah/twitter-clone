a01fda867e79ca187e2c37b2eb7ea9d7816800f2
Fajarullah <frullah12@gmail.com>
2021-11-21 23:38:19 +0700

    remove unncessary additional dropdown component

--- app/components/dropdown_list_component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class DropdownListComponent < ViewComponent::Base
-
-end
--- app/components/dropdown_list_item_component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class DropdownListItemComponent < ViewComponent::Base
-
-end
