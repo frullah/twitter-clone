aea06ba10cf75aec14861cea5a1f3ca3db5ee523
Fajarullah <frullah12@gmail.com>
2021-11-24 23:11:34 +0700

    refactor dropdown_content

--- app/components/dropdown_component.rb
+++ app/components/dropdown_component.rb
@@ -3,5 +3,10 @@
 class DropdownComponent < ViewComponent::Base
   # renders_one :dropdown_button, "ButtonOutlineComponent"
   renders_one :dropdown_button
-  renders_one :dropdown_content
+  renders_one :dropdown_content, ->(**options, &block) do
+    options[:class] = "dropdown-content #{options[:class]}"
+    content_tag(:div, **options) do
+      capture(&block)
+    end
+  end
 end
