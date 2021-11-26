394845e5f1b813c6a49e0eeed96e19177daa3c00
Fajarullah <frullah12@gmail.com>
2021-11-26 23:31:55 +0700

    refactor TweetCardComponent

--- app/components/tweet_card_component.rb
+++ app/components/tweet_card_component.rb
@@ -3,7 +3,19 @@
 class TweetCardComponent < ViewComponent::Base
   include Turbo::FramesHelper
 
-  def initialize(tweet:)
+  def initialize(tweet:, navigatable: false, **options)
     @tweet = tweet
+    @navigatable = true
+    @options = options
+  end
+
+  def before_render
+    if @navigatable
+      @options[:class] = "cursor-pointer hover:bg-light-300 #{@options[:class]}"
+      @options[:data] ||= {}
+      @options[:data][:controller] = "tweet #{@options[:data][:controller]}"
+      @options[:data][:tweet_url_value] = url_for(@tweet)
+      @options[:data][:action] = "click->tweet#openTweet #{@options[:data][:action]}"
+    end
   end
 end
